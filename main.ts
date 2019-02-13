79ca09621af1fbd07ddf10982525da68aefc7c00
gabriele.sato <gabriele.satp@persol.co.jp>
2019-02-13 11:34:30 +0900

    initial commit

---
+++ main.ts
@@ -0,0 +1,56 @@
+// tslint:disable-next-line: no-implicit-dependencies
+import { app, BrowserWindow } from 'electron';
+import * as isDev from 'electron-is-dev';
+import * as path from 'path';
+import * as url from 'url';
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+
+  console.error('Directory', __dirname, isDev);
+  // and load the index.html of the app.
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:4200'
+      : path.join(__dirname, '../www/index.html')
+  );
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
