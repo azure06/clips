aba802413294d236e30977167b5bd367d49ee871
Gabriele Sato <gabri06e@gmail.com>
2019-02-15 15:47:05 +0900

    minor refactoring

--- src/app/pages/clipboard-history/clipboard-history.page.ts
+++ src/app/pages/clipboard-history/clipboard-history.page.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-clipboard-page',
   templateUrl: './clipboard-history.page.html',
   styleUrls: ['./clipboard-history.page.scss']
 })
-export class ClipboardHistoryPage {}
+export class ClipboardHistoryPage {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  constructor() {}
+
+  loadData(event) {
+    // setTimeout(() => {
+    //   console.log('Done');
+    //   event.target.complete();
+
+    //   // App logic to determine if all data is loaded
+    //   // and disable the infinite scroll
+    //   if (data.length == 1000) {
+    //     event.target.disabled = true;
+    //   }
+    // }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
+}
