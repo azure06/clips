6e27a9b0fa6bcc0023fee310104eacfbdf345870
Gabriele Sato <gabri06e@gmail.com>
2019-02-22 06:10:29 +0900

    Delete electron.config.ts

--- src/electron/electron.config.ts
+++
@@ -1,8 +0,0 @@
-export const electronConfig = {
-  googleOAuth2: {
-    clientId:
-      '***REMOVED***',
-    clientSecret: '***REMOVED***',
-    scopes: ['https://www.googleapis.com/auth/drive.metadata.readonly']
-  }
-};
