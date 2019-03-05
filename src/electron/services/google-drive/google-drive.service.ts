b247495a7b1a2e8cae75c45f1b469ed1cf55e245
gabriele.sato <gabriele.satp@persol.co.jp>
2019-03-05 16:17:01 +0900

    minor update

--- src/electron/services/google-drive/google-drive.service.ts
+++ src/electron/services/google-drive/google-drive.service.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 import { OAuth2Client } from 'google-auth-library';
 // tslint:disable-next-line: no-submodule-imports
 import { drive_v3, google } from 'googleapis';
+import * as os from 'os';
+import * as path from 'path';
 
 export default class GoogleDriveService extends EventEmitter {
   private drive: drive_v3.Drive;
@@ -19,42 +21,61 @@ export default class GoogleDriveService extends EventEmitter {
     };
     const media = {
       mimeType: 'application/json',
-      body: fs.createReadStream('files/config.json')
+      body: fs.createReadStream('./tsconfig.json')
     };
-    this.drive.files.create(
-      ({
-        resource: fileMetadata,
-        media,
-        fields: 'id'
-      } as unknown) as any,
-      (err, file) => {
-        if (err) {
-          // Handle error
-          console.error(err);
-        } else {
-          console.log('Folder Id:', file.data.id);
-          this.drive.files.list(
-            {
-              spaces: 'appDataFolder',
-              fields: 'nextPageToken, files(id, name)',
-              pageSize: 100
-            },
-            // tslint:disable-next-line: no-shadowed-variable
-            (err, res) => {
-              if (err) {
-                // Handle error
-                console.error(err);
-              } else {
-                console.error(res.data.files);
-                // tslint:disable-next-line: no-shadowed-variable
-                res.data.files.forEach(file => {
-                  console.log('Found file:', file.name, file.id);
-                });
-              }
-            }
-          );
-        }
-      }
-    );
+
+    // this.drive.files.create(
+    //   ({
+    //     resource: fileMetadata,
+    //     media,
+    //     fields: 'id'
+    //   } as unknown) as any,
+    //   (err, file) => {
+    //     if (err) {
+    //       // Handle error
+    //       console.error(err);
+    //     } else {
+    //       console.log('Folder Id:', file.data.id);
+    //       this.drive.files.list(
+    //         {
+    //           spaces: 'appDataFolder',
+    //           fields: 'nextPageToken, files(id, name)',
+    //           pageSize: 100
+    //         },
+    //         // tslint:disable-next-line: no-shadowed-variable
+    //         (err, res) => {
+    //           if (err) {
+    //             // Handle error
+    //             console.error(err);
+    //           } else {
+    //             console.error(res.data.files);
+    //             const dest = fs.createWriteStream(
+    //               path.join(os.tmpdir(), 'hello23')
+    //             );
+    //             // tslint:disable-next-line: no-shadowed-variable
+    //             res.data.files.forEach(async file => {
+    //               console.log('Found file:', file.name, file.id);
+
+    //               const res2 = await this.drive.files.get(
+    //                 { fileId: file.id, alt: 'media' },
+    //                 { responseType: 'stream' }
+    //               );
+
+    //               (res2.data as any)
+    //                 .on('end', () => {
+    //                   console.log('Done downloading file.');
+    //                 })
+    //                 .on('error', err => {
+    //                   console.error('Error downloading file.');
+    //                 })
+    //                 .on('data', d => {})
+    //                 .pipe(dest);
+    //             });
+    //           }
+    //         }
+    //       );
+    //     }
+    //   }
+    // );
   }
 }
