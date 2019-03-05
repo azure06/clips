34979bbd593f7c38b2682e503764b829000943a0
Gabriele Sato <gabri06e@gmail.com>
2019-03-05 09:21:14 +0900

    add icon

--- src/electron/services/google-drive/google-drive.service.ts
+++ src/electron/services/google-drive/google-drive.service.ts
@@ -13,48 +13,48 @@ export default class GoogleDriveService extends EventEmitter {
   }
 
   private async init() {
-    // const fileMetadata = {
-    //   name: 'infiniti-clips',
-    //   parents: ['appDataFolder'],
-    //   mimeType: 'application/vnd.google-apps.folder'
+    const fileMetadata = {
+      name: 'infiniti-clips',
+      parents: ['appDataFolder'],
+      mimeType: 'application/vnd.google-apps.folder'
+    };
+    // const media = {
+    //   mimeType: 'application/json',
+    //   body: JSON.stringify({ app: 'Hello World ' })
     // };
-    // // const media = {
-    // //   mimeType: 'application/json',
-    // //   body: JSON.stringify({ app: 'Hello World ' })
-    // // };
-    // // this.googleOAuth2Client.projectId
-    // this.drive.files.create(
-    //   {
-    //     resource: fileMetadata,
-    //     // media,
-    //     fields: 'id'
-    //   },
-    //   (err, file) => {
-    //     if (err) {
-    //       // Handle error
-    //       console.error(err);
-    //     } else {
-    //       console.log('Folder Id:', file.data.id);
-    //       this.drive.files.list(
-    //         {
-    //           spaces: 'appDataFolder',
-    //           fields: 'nextPageToken, files(id, name)',
-    //           pageSize: 100
-    //         },
-    //         (err, res) => {
-    //           if (err) {
-    //             // Handle error
-    //             console.error(err);
-    //           } else {
-    //             console.error(res.data.files);
-    //             res.data.files.forEach(file => {
-    //               console.log('Found file:', file.name, file.id);
-    //             });
-    //           }
-    //         }
-    //       );
-    //     }
-    //   }
-    // );
+    // this.googleOAuth2Client.projectId
+    this.drive.files.create(
+      {
+        resource: fileMetadata,
+        // media,
+        fields: 'id'
+      },
+      (err, file) => {
+        if (err) {
+          // Handle error
+          console.error(err);
+        } else {
+          console.log('Folder Id:', file.data.id);
+          this.drive.files.list(
+            {
+              spaces: 'appDataFolder',
+              fields: 'nextPageToken, files(id, name)',
+              pageSize: 100
+            },
+            (err, res) => {
+              if (err) {
+                // Handle error
+                console.error(err);
+              } else {
+                console.error(res.data.files);
+                res.data.files.forEach(file => {
+                  console.log('Found file:', file.name, file.id);
+                });
+              }
+            }
+          );
+        }
+      }
+    );
   }
 }
