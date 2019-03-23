5865a3ce3056f0a12fe15dee684746bd828db6cd
Gabriele Sato <gabri06e@gmail.com>
2019-03-24 01:07:45 +0900

    sync

--- src/app/services/google-drive/google-drive.service.ts
+++ src/app/services/google-drive/google-drive.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Clip } from '../../models/models';
+import { ClipboardService } from '../clipboard/clipboard.service';
 import { ElectronService } from '../electron/electron.service';
 
 @Injectable()
 export class GoogleDriveService {
-  constructor(private es: ElectronService) {
+  constructor(
+    private es: ElectronService,
+    private clipboardService: ClipboardService
+  ) {
     this.es.ipcRenderer.on(
       'google-drive-change',
       (event, { data, error }: { data: { clips?: Clip[] }; error: any }) => {
-        console.error(data ? data.clips : error);
+        data
+          ? data.clips.forEach(clip => clipboardService.addClip(clip))
+          : console.error(error);
       }
     );
   }
