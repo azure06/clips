9745a727ee3e504f4aa4f62831c7082e53ff1b02
Gabriele Sato <gabri06e@gmail.com>
2019-03-28 21:52:48 +0900

    minor fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,13 +8,14 @@ sendGrid.setApiKey(environment.sendGridApiKey);
 // https://firebase.google.com/docs/functions/typescript
 export const sendContactMail = functions.https.onCall(
   async (data: any, context) => {
-    const { fullName, email, content } = data;
+    const { fullName, email, content, to } = data;
     const msgbody = {
-      to: 'info@infiniticlips.com',
-      from: 'info@infiniticlips.com',
+      to: to || 'info@infiniticlips.com',
+      from: email,
       subject: `Info - Contact us`,
       text: `${content}\n\nfrom ${fullName} - ${email}`,
-      html: `${content}<strong>from ${fullName} - ${email}</strong>`
+      html: `${content}
+      <strong>from ${fullName}</strong>`
     };
     return sendGrid
       .send(msgbody)
