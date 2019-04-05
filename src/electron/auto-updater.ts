5f90ac04e900837bfff2628f39cce1689cf9230c
Gabriele Sato <gabri06e@gmail.com>
2019-04-05 09:14:55 +0900

    minor fix

--- src/electron/auto-updater.ts
+++ src/electron/auto-updater.ts
@@ -1,35 +1,43 @@
 import { dialog } from 'electron';
+import * as isDev from 'electron-is-dev';
 import log from 'electron-log';
 import { autoUpdater } from 'electron-updater';
+const isDevelopment = (isDev as any).default;
 
-try {
-  setInterval(async () => {
-    const result = await autoUpdater.checkForUpdates();
-    log.info(result);
-  }, 5000);
+!isDevelopment
+  ? (() => {
+      setInterval(async () => {
+        const result = await autoUpdater
+          .checkForUpdatesAndNotify()
+          .catch(err => {
+            log.error(err);
+          });
+        log.info(result);
+      }, 5000);
 
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail:
-        'A new version has been downloaded. Restart the application to apply the updates.'
-    };
+      autoUpdater.on(
+        'update-downloaded',
+        (event, releaseNotes, releaseName) => {
+          const dialogOpts = {
+            type: 'info',
+            buttons: ['Restart', 'Later'],
+            title: 'Application Update',
+            message: process.platform === 'win32' ? releaseNotes : releaseName,
+            detail:
+              'A new version has been downloaded. Restart the application to apply the updates.'
+          };
 
-    dialog.showMessageBox(dialogOpts, response => {
-      if (response === 0) {
-        autoUpdater.quitAndInstall();
-      }
-    });
-  });
+          dialog.showMessageBox(dialogOpts, response => {
+            if (response === 0) {
+              autoUpdater.quitAndInstall();
+            }
+          });
+        }
+      );
 
-  autoUpdater.on('error', message => {
-    console.error('There was a problem updating the application');
-    console.error(message);
-    log.warn(message);
-  });
-} catch (err) {
-  log.warn(err);
-}
+      autoUpdater.on('error', message => {
+        console.error('There was a problem updating the application');
+        log.warn(message);
+      });
+    })()
+  : (() => console.log('Autoupdater not available'))();
