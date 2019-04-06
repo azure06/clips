e0834d3c35daf40431b0b5f10ce67b32437e84e7
Gabriele Sato <gabri06e@gmail.com>
2019-04-06 19:01:51 +0900

    add monospace font

--- src/app/pages/editor/editor.page.ts
+++ src/app/pages/editor/editor.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Quill from 'quill';
 
 const Delta = Quill.import('delta');
@@ -8,23 +8,29 @@ const Delta = Quill.import('delta');
   templateUrl: './editor.page.html',
   styleUrls: ['./editor.page.scss']
 })
-export class EditorPage {
+export class EditorPage implements OnInit {
+  title: '';
   editor: Quill;
   change = new Delta();
   constructor() {}
 
-  ionViewDidEnter(): void {
+  ngOnInit(): void {
     this.editor = new Quill('#editor-container', {
       modules: {
         toolbar: [
-          [{ header: [1, 2, false] }],
+          // [{ header: [1, 2, false] }],
           ['bold', 'italic', 'underline'],
           ['image', 'code-block']
         ]
       },
-      placeholder: 'Compose an epic...',
-      theme: 'snow' // or 'bubble'
+      placeholder: 'Compose...',
+      theme: 'bubble' // or 'bubble'
     });
+
+    const changes = localStorage.getItem('untitled');
+    if (changes) {
+      this.change = this.editor.setContents(JSON.parse(changes));
+    }
 
     this.editor.on('text-change', delta => {
       this.change = this.change.compose(delta);
@@ -34,19 +40,24 @@ export class EditorPage {
     setInterval(() => {
       if (this.change.length() > 0) {
         console.log('Saving changes', this.change);
-        /*
-    Send partial changes
-    $.post('/your-endpoint', {
-      partial: JSON.stringify(change)
-    });
+        /*     Send partial changes */
+        // $.post('/your-endpoint', {
+        //   partial: JSON.stringify(change)
+        // });
 
-    Send entire document
-    $.post('/your-endpoint', {
-      doc: JSON.stringify(quill.getContents())
-    });
-    */
+        localStorage.setItem(
+          this.title || 'untitled',
+          JSON.stringify(this.editor.getContents())
+        );
         this.change = new Delta();
       }
     }, 5 * 1000);
+  }
+
+  ionViewWillLeave() {
+    localStorage.setItem(
+      this.title || 'untitled',
+      JSON.stringify(this.editor.getContents())
+    );
   }
 }
