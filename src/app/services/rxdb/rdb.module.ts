b4811e228c1ccb2af485b0e08687fca7d8afdc07
Gabri <gabriele.sato@rakuten.com>
2019-10-04 17:55:03 +0900

    add rxdb

---
+++ src/app/services/rxdb/rdb.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RxDBService } from './rdb.service';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: [RxDBService],
+  bootstrap: []
+})
+export class RxDBServiceModule {}
