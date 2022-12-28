9d984accc70da0a1c314c20a33aec8e591c85786
Gabriele Sato <gabri06e@gmail.com>
2022-12-28 16:11:07 +0900

    Bump up version to 0.2.7

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
         productName: 'Clips',
         publish: ['github'],
         afterSign: 'scripts/notarize.js',
-        buildVersion: '0.2.6',
+        buildVersion: '0.2.7',
         // win: {
         //   publisherName: ['Gabriele Sato'],
         //   target: ['nsis'],
